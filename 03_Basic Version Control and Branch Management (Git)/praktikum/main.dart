13993b32d16cc16d85164977e3a4edc28f6ce51a
akito24 <68215677+akito24@users.noreply.github.com>
2022-08-23 18:57:23 +0700

    Fix eror

--- 03_Basic Version Control and Branch Management (Git)/praktikum/main.dart
+++ 03_Basic Version Control and Branch Management (Git)/praktikum/main.dart
@@ -3,9 +3,5 @@ void main() {
   print('Update A');
   print('Update B');
   print('Update C');
-<<<<<<< HEAD
-  print('Conflict A');
-=======
   print('Conflict B');
->>>>>>> e312ab66c0e2eb9307af1ff1bbf8705a1353aa91
 }
