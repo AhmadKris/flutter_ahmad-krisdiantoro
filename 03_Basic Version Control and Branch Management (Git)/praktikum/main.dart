bafb306ca20c14dfd53b76ba520617f9a37f020b
akito24 <68215677+akito24@users.noreply.github.com>
2022-08-23 16:16:16 +0700

    Fixing B on main.dart

--- 03_Basic Version Control and Branch Management (Git)/praktikum/main.dart
+++ 03_Basic Version Control and Branch Management (Git)/praktikum/main.dart
@@ -1,8 +1,15 @@
 void main() {
   print('git status, git add, git commit');
+<<<<<<< Updated upstream
   print('git pull (server) (branch)');
   print('git branch');
   print('A');
   print('B');
+=======
+  print('git push (server) (branch)');
+  print('git brach');
+  print('A');
+  print('D');
+>>>>>>> Stashed changes
   print('C');
 }
