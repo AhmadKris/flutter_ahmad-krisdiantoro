3475a529aefe8f2bcb67dcbe5eaba92a9edba8ce
Ahmad Krisdiantoro <achmadcrizdiantho10@gmail.com>
2022-08-29 17:16:43 +0700

    update all

---
+++ 06_ Basic Dart Programming/praktikum/kalimat.dart
@@ -0,0 +1,7 @@
+void main() {
+  String nama = "Ahmad Krisdiantoro";
+  String fakultas = "Fakultas Teknologi Informasi";
+  String progdi = "Teknik Informatika";
+
+  print("Nama $nama Dari $fakultas Program Study $progdi");
+}
