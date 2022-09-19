6f7b9e392b2a05b67ded35a7b57531e7892cdc5b
Ahmad Krisdiantoro <60264253+AhmadKris@users.noreply.github.com>
2022-09-20 01:14:30 +0700

    Delete 14_Flutter CLI and Flutter package management directory

--- 14_Flutter CLI and Flutter package management/praktikum/flutter_packages/android/app/src/main/kotlin/com/example/flutter_packages/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_packages
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- 14_Flutter CLI and Flutter package management/praktikum/flutter_packages/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-// import './task01.dart';
-import './task02.dart';
-
-void main() {
-  runApp(const MyApp());
-}
